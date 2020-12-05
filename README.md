be97fd577c118a036894522953db84d738d3516d
mcampos-quinn <mcq@berkeley.edu>
2020-12-04 22:23:43 -0800

    now it runs siegfried on every file, then optionally mediainfo too; its slow so will rethink approach

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # fileReporter
 
-Command-line script to output a csv listing directories, filenames, file sizes, and created/modified times.
+Script to output a CSV inventory of a directory and subdirectories. It lists the subdirectory paths and information about each file:
+
+* Filename
+* Human-readable filesize
+* Created time (according to filesystem)
+* Last modified time
+* Format according to [Siegfried](https://www.itforarchivists.com/siegfried/)
+
+
+ listing directories, filenames, file sizes, and created/modified times.
 `/path/to/dirToReportOn` --> `/output/path/dirToReportOn.csv`
-Is simple, yes? Tested with Python 3 on Mac.
+
+
--- fileReporter.py
+++ fileReporter.py
@@ -4,16 +4,47 @@ import argparse
 import ast
 import csv
 from datetime import datetime
+import json
 import os
+import pyjq
 import subprocess
 import sys
 import time
+
+av_detail_dict = {
+	# 'CSV header name':'__Mediainfo Field Name',
+	'File Format (MediaInfo)':'Format',
+	'Format term':'Format_String',
+	'Duration':'Duration_String',
+	'Format Profile':'Format_Profile',
+	'Sample Rate':'SamplingRate_String',
+	'Bit Depth':'BitDepth_String',
+	'Bitrate':'BitRate_String',
+	'Channels':'Channels',
+	'Frame rate':'FrameRate_String',
+	'Width':'Width_String',
+	'Height':'Height_String',
+	'CodecID':'CodecID',
+	'Aspect ratio':'DisplayAspectRatio_String',
+	'Audio Channels':'AudioCount'
+}
+
+general_siegfried_dict = {
+		"File Name":[],
+		"File Size":[],
+		"File Format (Siegfried)":[],
+		"Folder":[],
+		"Created On":[],
+		"Last Modified On":[],
+		"Full Siegfried Output":[],
+		"Siegfried Error Messages":[]
+	}
 
 def set_args():
 	parser = argparse.ArgumentParser()
 	desktop = os.path.expanduser("~/Desktop")
 	parser.add_argument(
-		'-p','--inventoryPath',
+		'-p','--inventory_path',
 		help=(
 			"Path to the root directory you want to inventory.\n"
 			"Please make it the full filesystem path!"
@@ -21,23 +52,91 @@ def set_args():
 		required=True
 		)
 	parser.add_argument(
-		'-o','--outPath',
+		'-o','--out_path',
 		default=desktop,
 		help=("Output path for the inventory CSV. Default is `~/Desktop`."
 			),
 		required=False
 		)
 	parser.add_argument(
-		'-t','--mimeType',
-		default='video/mp4',
-		help=("Set the mimeType you want to declare for all the files on upload.\n"
-			"Default is 'mp4.' You can add more options in `mimeTypes.json` in "
-			"this directory."
+		'-m','--mediainfo',
+		action='store_true',
+		default=False,
+		help=("Run mediainfo on input files to get details of AV files.\n"
+			"Default is False."
 			),
 		required=False
 		)
 
 	return parser.parse_args()
+
+
+def av_sniffer(input_path):
+	'''
+	Run `mediainfo` on a file to see if it's AV.
+	Return audio/video/image/not_av along with mediainfo output JSON
+	'''
+	mediainfo_command = [
+		'mediainfo',
+		'-f',
+		'--Output=JSON',
+		input_path
+		]
+
+	try:
+		mediainfo_out = subprocess.run(mediainfo_command,stdout=subprocess.PIPE)
+		out = mediainfo_out.stdout.decode('utf-8')
+		# print(out)
+		output = json.loads(out)
+
+		general_track = pyjq.first(
+			'.media.track[] | select(.["@type"] == "General")',
+			output
+			)
+		if 'VideoCount' in general_track:
+			format = 'Video'
+		elif 'AudioCount' in general_track:
+			format = 'Audio'
+		elif 'ImageCount' in general_track:
+			format = 'Image'
+		else:
+			output = None
+			format = 'not_av'
+	except:
+		output = None
+		format = None
+
+	return output,format
+
+def av_details(mediainfo_output,input_format):
+	details = av_detail_dict
+	temp = {}
+	for field in details.keys():
+		try:
+			temp[field] = pyjq.first(
+				'.media.track[] | select(."@type" == "{}") | .{}'.format(
+					input_format,details[field]
+					),
+				mediainfo_output
+				)
+			# print(field)
+		except:
+			pass
+	if input_format == 'Video':
+		try:
+			temp['Audio Channels'] = pyjq.first(
+				'.media.track[] | select(."@type" == "General") | .AudioCount',
+				mediainfo_output
+				)
+		except:
+			pass
+
+	filtered = {k: v for k, v in temp.items() if v is not None}
+	details = filtered
+	# print(details)
+
+	return details
+
 
 def humansize(nbytes):
 	suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
@@ -50,11 +149,11 @@ def humansize(nbytes):
 
 	return '%s %s' % (f, suffixes[i])
 
-def file_signature(filePath):
+def file_signature(file_path):
 	command = [
 	"sf",
 	"-json",
-	filePath
+	file_path
 	]
 
 	output = subprocess.run(
@@ -65,105 +164,152 @@ def file_signature(filePath):
 
 	format = ""
 	error = ""
-	fullSiegfried = ""
+	full_Siegfried = ""
 
-	fullSiegfried = output.stdout.decode()
+	full_Siegfried = output.stdout.decode()
 	error = output.stderr.decode()
-	format = ast.literal_eval(
-		output.stdout.decode()
-		)['files'][0]['matches'][0]['format']
-
+	format = pyjq.first(
+		'.files[].matches[].format',
+		json.loads(full_Siegfried)
+		)
+	# If no format was returned, look for 'id'
 	if format == "":
-		format = ast.literal_eval(
-		output.stdout.decode()
-		)['files'][0]['matches'][0]['warning']
+		format = pyjq.first(
+		'.files[].matches[].id',
+		json.loads(full_Siegfried)
+		)
+	# If that fails too, look for 'warning'
+	if format == "":
+		format = pyjq.first(
+		'.files[].matches[].warning',
+		json.loads(full_Siegfried)
+		)
 
-	print(format)
+	return format, error, full_Siegfried
 
-	return format, error, fullSiegfried
-
-def inventory(inventoryPath):
-	folder = ["Folder"]
-	fileName = ["File Name"]
-	fileSize = ["File Size"]
-	createdTime = ["Created On"]
-	modTime = ["Last Modified On"]
-	fileFormat = ["File Format"]
-	fullSiegfriedJSON = ["Full Siegfried Output"]
-	siegfriedError = ["Siegfried Error Messages"]
+def inventory(inventory_path,mediainfo):
+	# initiate a list for dicts about each file
+	inventory_dicts = []
+	# get the general dict of siegfried details
+	sf_details = general_siegfried_dict
 	
-	for root, dirs, files in os.walk(inventoryPath):
+	if mediainfo:
+		# add empty values for AV information
+		for k in av_detail_dict.keys():
+			sf_details[k] = ""
+
+	for root, dirs, files in os.walk(inventory_path):
 		for _file in files:
+			print(_file)
 			if not _file.startswith('.'):
-				filePath = os.path.join(root, _file)
+				# Start an individual dict for each file
+				item_details = sf_details
+
+				file_path = os.path.join(root, _file)
 				
 				# GET THE FILE SIGNATURE
-				format, sfError, fullSiegfried = file_signature(filePath)				
+				format, sf_Error, full_Siegfried = file_signature(file_path)				
 				
 				# GET THE FILE SIZE
-				size = os.stat(filePath).st_size
+				size = os.stat(file_path).st_size
 				hsize = humansize(size)
 				
 				# GET CREATED/MODIFIED  DATES
-				created = os.stat(filePath).st_birthtime
+				created = os.stat(file_path).st_birthtime
 				localCreated = time.localtime(created)
 				iso8601Created = time.strftime("%Y-%m-%dT%H:%M:%S",localCreated)
-				modded = os.path.getmtime(filePath)
+				
+				modded = os.path.getmtime(file_path)
 				localModded = time.localtime(modded)
 				iso8601Modded = time.strftime("%Y-%m-%dT%H:%M:%S",localModded)
-				
 
-				# APPEND ALL THE VALUES TO EACH COLUMN
-				folder.append(os.path.dirname(filePath))
-				fileName.append(_file)
-				fileSize.append(hsize)
-				createdTime.append(iso8601Created)
-				modTime.append(iso8601Modded)
-				fileFormat.append(format)
-				fullSiegfriedJSON.append(fullSiegfried)
-				siegfriedError.append(sfError)
-	
-	# TURN EACH LIST INTO A ROW FOR CSV	
-	inventoryRows = zip(
-		folder,
-		fileName,
-		fileSize,
-		createdTime,
-		modTime,
-		fileFormat,
-		fullSiegfriedJSON,
-		siegfriedError
-		)
+				# ASSIGN VALUES TO EACH FIELD
+				item_details["Folder"] = os.path.dirname(file_path)
+				item_details["File Name"] = _file
+				item_details["File Size"] = hsize
+				item_details["Created On"] = iso8601Created
+				item_details["Last Modified On"] = iso8601Modded
+				item_details["File Format (Siegfried)"] = format
+				item_details["Full Siegfried Output"] = full_Siegfried
+				item_details["Siegfried Error Messages"] = sf_Error
 
-	return inventoryRows
+				# RUN MEDIAINFO ON AV STUFF
+				if mediainfo:
+					output,av_format = av_sniffer(file_path)
+					av_file_details = av_details(output,av_format)
+					# add mediainfo info to item dict
+					for k,v in av_file_details.items():
+						item_details[k] = v
+					# or if the value was absent, append empty string
+					for k in av_detail_dict.keys():
+						if not k in av_file_details:
+							# print(sf_details)
+							item_details[k] = ""
 
-def write_inventory(inventoryRows,inventoryPathDirname,outPath):
-	csvPath = os.path.join(outPath,inventoryPathDirname) + '.csv'
-	with open(csvPath,'a') as outfile:
-		writer = csv.writer(outfile)
-		for row in inventoryRows:
-			writer.writerow(row)
-	return csvPath
+				# print(item_details)
+				# add the item to the inventory
+				inventory_dicts.append(item_details.copy())
+	# print(inventory_dicts)
+
+	return inventory_dicts
+
+def write_inventory(
+	inventory_dicts,
+	inventory_path_dirname,
+	out_path,
+	mediainfo
+	):
+	header_order = [
+	'File Name', 'File Size', 'File Format (Siegfried)', 'Folder', 
+	'Created On', 'Last Modified On',  'File Format (MediaInfo)', 
+	'Format term', 'Duration', 'Format Profile', 'Sample Rate', 'Bit Depth', 
+	'Bitrate', 'Channels', 'Frame rate', 'Width', 'Height', 'CodecID', 
+	'Aspect ratio', 'Audio Channels','Full Siegfried Output',
+	'Siegfried Error Messages','Full Mediainfo Output'
+	]
+	# Get rid of unused columns
+	if not mediainfo:
+		for i in header_order:
+			if i in av_detail_dict:
+				header_order.remove(i)
+
+	csv_path = os.path.join(out_path,inventory_path_dirname) + '.csv'
+	with open(csv_path,'a') as outfile:
+		writer = csv.DictWriter(outfile,fieldnames=header_order)
+		writer.writeheader()
+		for item in inventory_dicts:
+			# print(item)
+			writer.writerow(item)
+
+	return csv_path
 
 def main():
 	args = set_args()
-	inventoryPath = args.inventoryPath
-	inventoryPathDirname = os.path.basename(inventoryPath)
-	outPath = args.outPath
+	inventory_path = args.inventory_path
+	inventory_path_dirname = os.path.basename(inventory_path)
+	out_path = args.out_path
+	mediainfo = args.mediainfo
 
-	if not os.path.isdir(inventoryPath):
+	if not os.path.isdir(inventory_path):
 		print(
 			"Your selected input path ({}) is not a valid path!".format(
-				inventoryPath
+				inventory_path
 				)
 			)
 		sys.exit()
 	else:
-		inventoryRows = inventory(inventoryPath)
+		# go make the inventory
+		inventory_dicts = inventory(inventory_path,mediainfo)
 
-	csvPath = write_inventory(inventoryRows,inventoryPathDirname,outPath)
+	# write the inventory to csv
+	csv_path = write_inventory(
+		inventory_dicts,
+		inventory_path_dirname,
+		out_path,
+		mediainfo
+		)
 
-	print("you should now have an inventory CSV file at {}".format(csvPath))
+	print("you should now have an inventory CSV file at {}".format(csv_path))
 
 if __name__ == '__main__':
 	main()
